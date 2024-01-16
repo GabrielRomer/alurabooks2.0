863604f6f36e570fc9b893a205bd61d2e9fdf396
GabrielRomer <139795558+GabrielRomer@users.noreply.github.com>
2024-01-16 14:42:24 -0300

    Add files via upload

---
+++ app/metodoForEach.js
@@ -0,0 +1,35 @@
+const elementoParaInserirLivros = document.getElementById('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('')
+
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = ''
+    listaDeLivros.forEach(livro => {
+        //let disponibilidade = verificarDisponibilidadeDoLivro (livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+        <img class="${disponibilidade}" src="${livro.imagem}"
+          alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+          ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+          <span class="tag">${livro.categoria}</span>
+        </div>
+      </div>
+        `
+    })
+}
+
+
+//opção para verificar a disponibilidade usando uma função. da forma que está executando usou-se apenas 1 linha para a mesma função. 
+/*function verificarDisponibilidadeDoLivro(livro){ 
+    if (livro.quantidade > 0){
+        return 'livro__imagens'
+    }else {
+        return 'livros__imagens indisponivel'
+    }
+}*/
